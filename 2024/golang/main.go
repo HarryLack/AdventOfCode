bbe5e0c28c7a1c1033baba29c6c925bd49906902
Harry B-L <harrylackdev@gmail.com>
2025-09-20 20:11:09 +0100

    2024 Go solver utils

--- 2024/golang/main.go
+++ 2024/golang/main.go
@@ -1,41 +1,78 @@
 package main
 
 import (
-	"aoc-2024/day01"
-	"aoc-2024/day02"
-	"aoc-2024/day03"
-	"aoc-2024/day04"
-	"aoc-2024/day05"
-	"aoc-2024/day06"
-	"aoc-2024/day07"
+	day1 "aoc-2024/day01"
+	day2 "aoc-2024/day02"
+	day3 "aoc-2024/day03"
+	day4 "aoc-2024/day04"
+	day5 "aoc-2024/day05"
+	day6 "aoc-2024/day06"
+	day7 "aoc-2024/day07"
+
+	// day8 "aoc-2024/day08"
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
+	args := os.Args
+
 	start := time.Now()
-	prev := start
-	day1.Solve()
-	fmt.Printf("=== %v ===\n", time.Since(prev))
-	prev = time.Now()
-	day2.Solve()
-	fmt.Printf("=== %v ===\n", time.Since(prev))
-	prev = time.Now()
-	day3.Solve()
-	fmt.Printf("=== %v ===\n", time.Since(prev))
-	prev = time.Now()
-	day4.Solve()
-	fmt.Printf("=== %v ===\n", time.Since(prev))
-	prev = time.Now()
-	day5.Solve()
-	fmt.Printf("=== %v ===\n", time.Since(prev))
-	prev = time.Now()
-	day6.Solve()
-	fmt.Printf("=== %v ===\n", time.Since(prev))
-	prev = time.Now()
-	day7.Solve()
-	fmt.Printf("=== %v ===\n", time.Since(prev))
-	prev = time.Now()
+	if len(args) == 2 {
+		switch args[1] {
+		case "1":
+			day1.Solve()
+			fmt.Printf("=== %v ===\n", time.Since(start))
+		case "2":
+			day2.Solve()
+			fmt.Printf("=== %v ===\n", time.Since(start))
+		case "3":
+			day3.Solve()
+			fmt.Printf("=== %v ===\n", time.Since(start))
+		case "4":
+			day4.Solve()
+			fmt.Printf("=== %v ===\n", time.Since(start))
+		case "5":
+			day5.Solve()
+			fmt.Printf("=== %v ===\n", time.Since(start))
+		case "6":
+			day6.Solve()
+			fmt.Printf("=== %v ===\n", time.Since(start))
+		case "7":
+			day7.Solve()
+			fmt.Printf("=== %v ===\n", time.Since(start))
+			// case "8":
+			// 	day8.Solve()
+			// 	fmt.Printf("=== %v ===\n", time.Since(start))
+		}
+	} else {
+		prev := start
+		day1.Solve()
+		fmt.Printf("=== %v ===\n", time.Since(prev))
+		prev = time.Now()
+		day2.Solve()
+		fmt.Printf("=== %v ===\n", time.Since(prev))
+		prev = time.Now()
+		day3.Solve()
+		fmt.Printf("=== %v ===\n", time.Since(prev))
+		prev = time.Now()
+		day4.Solve()
+		fmt.Printf("=== %v ===\n", time.Since(prev))
+		prev = time.Now()
+		day5.Solve()
+		fmt.Printf("=== %v ===\n", time.Since(prev))
+		prev = time.Now()
+		day6.Solve()
+		fmt.Printf("=== %v ===\n", time.Since(prev))
+		prev = time.Now()
+		day7.Solve()
+		fmt.Printf("=== %v ===\n", time.Since(prev))
+		prev = time.Now()
+		// day8.Solve()
+		// fmt.Printf("=== %v ===\n", time.Since(prev))
+		// prev = time.Now()
+	}
 
 	fmt.Printf("\nTotal: %v\n", time.Since(start))
 }
